7cfa419febaec72407563b9b0f89aa98b994231b
sammybaba <146319195+sammybaba@users.noreply.github.com>
2023-09-28 11:39:28 +0500

    Add files via upload

---
+++ 1lec.php
@@ -0,0 +1,14 @@
+<?php
+$x=24;
+$y=34;
+
+echo "The Sum  of ".$x."+".$y." is ".$x+$y; 
+echo "<br>";
+echo "The Subtraction  of ".$x."-".$y." is ".$x-$y;
+echo "<br>";
+echo "The multiplication  of ".$x."*".$y." is ".$x*$y;
+echo "<br>";
+echo "The  divison of ".$x."/".$y." is ".$x/$y;
+echo "<br>";
+
+?>
